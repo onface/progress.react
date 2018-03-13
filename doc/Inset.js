204a1a0b1a5120bdf6b32d47ea4a636e29db3a4b
Grifree <1149290648@qq.com>
2018-03-13 11:22:51 +0800

    calc height ->30

--- doc/Inset.js
+++ doc/Inset.js
@@ -6,19 +6,19 @@ class Inset extends React.Component {
             <div className="insetDemo"
 	            style={{width:'300px',fontSize:'16px'}} 
             >
-                <Progress type={'inset'} insetText={true} percent={20} /> <br/>
-                <Progress type={'inset'} insetText={true} percent={40} active={true} /> <br/>
-                <Progress type={'inset'} insetText={true} percent={60} info={false} /> <br/>
+                <Progress type={'inset'} insetText={true} percent={20} /> 
+                <Progress type={'inset'} insetText={true} percent={40} active={true} /> 
+                <Progress type={'inset'} insetText={true} percent={60} info={false} /> 
                 <Progress type={'inset'} insetText={true} percent={80} status={'error'} 
                 	info={function(percent){
                 		return (<span className="fi fi-times-circle"> 失败</span>)
                 	}}
-                /> <br/>
+                /> 
                 <Progress type={'inset'} percent={100} status={'success'} 
                     insetText={function(percent){
                         return (<span className="fi fi-check-circle"></span>)
                     }}
-                /> <br/>
+                /> 
                 <Progress type={'inset'} percent={85.24564} accuracy={2}
                     insetText={function(percent){
                         return (<span>加载中 : {percent}%</span>)
